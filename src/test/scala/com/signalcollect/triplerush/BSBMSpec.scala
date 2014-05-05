11d9c61da6446bd6440790c31f9e5da5a12f37c7
Bibek <paudel.bibek@gmail.com>
2014-05-05 13:42:06 +0200

    Added test cases for dictionary decoding

--- src/test/scala/com/signalcollect/triplerush/BSBMSpec.scala
+++ src/test/scala/com/signalcollect/triplerush/BSBMSpec.scala
@@ -6,10 +6,10 @@ import com.signalcollect.triplerush.sparql.Sparql
 import com.signalcollect.triplerush.evaluation.BerlinSparqlQueries
 import com.signalcollect.triplerush.evaluation.BerlinQueriesParameterized7
 import com.signalcollect.triplerush.evaluation.BerlinSparqlParameterized100
+import com.signalcollect.triplerush.TRAdaptor
 
 class BSBMSpec extends FlatSpec with Matchers {
 
-  /*
   "Parser" should "successfully parse BSBM query" in {
     val tr = new TripleRush
     try {
@@ -20,9 +20,9 @@ class BSBMSpec extends FlatSpec with Matchers {
       jena.loadNtriples("./bsbm_7.nt")
 
       for ((queryId, queries) <- BerlinQueriesParameterized7.queries) {
-        for(subQueryId <- 1 to 25){
-        //for (q <- queries) {
-        val q = queries(subQueryId)
+        for (subQueryId <- 1 to 25) {
+          //for (q <- queries) {
+          val q = queries(subQueryId)
           val trResults = trA.execute(q).toSet
           val jenaResults = jena.execute(q).toSet
           if (trResults != jenaResults) {
@@ -38,13 +38,11 @@ class BSBMSpec extends FlatSpec with Matchers {
     }
   }
 
-  */
-
-  
+  /*
   "Parser" should "successfully parse another BSBM query" in {
     val tr = new TripleRush
     try {
-            val trA = TRAdaptor(tr)
+      val trA = TRAdaptor(tr)
       trA.loadNtriples("./bsbm_7.nt")
 
       val queries = BerlinQueriesParameterized7.queries(5)
@@ -56,8 +54,33 @@ class BSBMSpec extends FlatSpec with Matchers {
     } finally {
       tr.shutdown
     }
+  }*/
+
+  "Parser" should "properly decode results" in {
+    val tr = new TripleRush
+    try {
+      val trA = TRAdaptor(tr)
+      val jena = new JenaAdaptor()
+      trA.loadNtriples("./bsbm_7.nt")
+      jena.loadNtriples("./bsbm_7.nt")
+
+      for ((queryId, queries) <- BerlinQueriesParameterized7.queries) {
+        for (subQueryId <- 1 to 25) {
+          val query = queries(subQueryId)
+          val trResults = trA.getBindings(query)
+          val jenaResults = jena.getBindings(query)
+          
+          val trResultCount = trA.execute(query).toSet.size
+          val jenaResultCount = jena.execute(query).toSet.size
+          //println(s"queryId $queryId, subQueryId: $subQueryId, TRresultCount: $trResultCount, jenaresultcount: $jenaResultCount, trstrlen: $trResults, jenastrlen: $jenaResults")
+          assert(trResults === jenaResults)
+        }
+      }
+    } finally {
+      tr.shutdown
+    }
   }
-  
+
 }
 
 // Map(property -> http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/validTo, hasValue -> 2008-05-30T00:00:00, isValueOf -> *), Map(property -> http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/price, hasValue -> 7792.07, isValueOf -> *), Map(property -> http://purl.org/dc/elements/1.1/date, hasValue -> 2008-03-18, isValueOf -> *))
--- src/test/scala/com/signalcollect/triplerush/TestStore.scala
+++ src/test/scala/com/signalcollect/triplerush/TestStore.scala
@@ -26,14 +26,37 @@ case class TRAdaptor(tr: TripleRush = new TripleRush) extends TestStore {
     parsedQuery match {
       case None => List.empty
       case Some(p) =>
-        val selectVariableStrings = p.selectVariableIds.map(x => p.idToVariableName(-x-1))
+        val selectVariableStrings = p.selectVariableIds.map(x => p.idToVariableName(-x - 1))
         p.resultIterator.map {
           result =>
             selectVariableStrings.map(bindingName => (bindingName, result(bindingName))).toMap
         }.toList
     }
   }
-  
+
+  def getBindings(sparql: String) = {
+    val parsedQuery = Sparql(sparql)(tr)
+    val resultMap = List.empty[Map[Int, String]]
+    var stringLength = 0
+    val dict = tr.dictionary
+    parsedQuery match {
+      case None => List.empty
+      case Some(p) =>
+        val resultIterator = p.encodedResults
+        var count = 0
+        while (resultIterator.hasNext) {
+          val res = resultIterator.next
+          var arrayIndex = 0
+          while (arrayIndex < res.length) {
+            stringLength += dict.unsafeDecode(res(arrayIndex)).length()
+            arrayIndex += 1
+          }
+          count += 1
+        }
+    }
+    
+    stringLength
+  }
 }
 
 case class JenaAdaptor() extends TestStore {
@@ -51,7 +74,7 @@ case class JenaAdaptor() extends TestStore {
     val results = qe.execSelect
     results.map {
       result =>
-        selectVariableStrings.map{
+        selectVariableStrings.map {
           bindingName =>
             val bindingAsString = result.get(bindingName) match {
               case res: Resource => res.toString
@@ -59,8 +82,34 @@ case class JenaAdaptor() extends TestStore {
               case null => "*"
             }
             (bindingName, bindingAsString)
-          }.toMap
+        }.toMap
     }.toList
   }
-  
+
+  def getBindings(sparql: String): Int = {
+    import collection.JavaConversions._
+    val parsedQuery = SparqlParser.parse(sparql)
+    val selectVariableStrings = parsedQuery.select.selectVariableNames
+    val query = QueryFactory.create(sparql)
+    val qe = QueryExecutionFactory.create(query, jena)
+    val results = qe.execSelect
+
+    var stringLength = 0
+    
+    results.map {
+      result =>
+        selectVariableStrings.map {
+          bindingName =>
+            val bindingAsString = result.get(bindingName) match {
+              case res: Resource => stringLength += res.toString.length()
+              case lit: Literal => stringLength += lit.getLexicalForm.length()
+              case null => stringLength += 1
+            }
+            (bindingName, bindingAsString)
+        }.toMap
+    }.toList
+    
+    stringLength
+  }
+
 }
