721f460683a48728008b188a85f159288e54424f
Bibek <paudel.bibek@gmail.com>
2014-05-06 10:43:07 +0200

    We used this for distributed TR BSBM evaluation.

--- src/main/scala/com/signalcollect/triplerush/evaluation/BerlinSparqlEvaluation.scala
+++ src/main/scala/com/signalcollect/triplerush/evaluation/BerlinSparqlEvaluation.scala
@@ -37,7 +37,7 @@ class BerlinSparqlEvaluation extends SlurmDeployableAlgorithm {
     val optimizerCreatorName = parameters("optimizerCreator")
     val datasetSize = parameters("universities")
     val optimizerCreator = getObject[Function1[TripleRush, Option[Optimizer]]](optimizerCreatorName)
-    val warmupRuns = parameters("warmupRuns").toInt
+    val warmupRuns = parameters("jitRepetitions").toInt
 
     val graphBuilder = GraphBuilder.withPreallocatedNodes(nodeActors)
     val tr = new TripleRush(graphBuilder)
@@ -45,6 +45,7 @@ class BerlinSparqlEvaluation extends SlurmDeployableAlgorithm {
 
     var commonResults = parameters
     commonResults += "numberOfNodes" -> tr.graph.numberOfNodes.toString
+    commonResults += "jitRepetitions" -> warmupRuns.toString
     commonResults += "numberOfWorkers" -> tr.graph.numberOfWorkers.toString
     commonResults += "java.runtime.version" -> System.getProperty("java.runtime.version")
 
@@ -53,19 +54,23 @@ class BerlinSparqlEvaluation extends SlurmDeployableAlgorithm {
 
     val loadingTime = measureTime {
       tr.loadNtriples(ntriplesFileLocation, Some(0))
+      tr.awaitIdle
+    }
+
+    println(s"Finished loading")
+    
+    val optimizerInitialisationTime = measureTime {
       tr.prepareExecution
     }
 
+    println(s"Finished optimizer initialization")
+    
     JvmWarmup.sleepUntilGcInactiveForXSeconds(60, 180)
 
-    val optimizerInitStart = System.nanoTime
-    val optimizer = optimizerCreator(tr)
-    val optimizerInitEnd = System.nanoTime
-
-    commonResults += ((s"optimizerInitialisationTime", roundToMillisecondFraction(optimizerInitEnd - optimizerInitStart).toString))
-    commonResults += ((s"optimizerName", optimizer.toString))
+    commonResults += ((s"optimizerInitialisationTime", optimizerInitialisationTime.toString))
+    commonResults += ((s"optimizerName", "ExplorationOptimizer"))
     commonResults += (("loadingTime", loadingTime.toString))
-    //commonResults += s"loadNumber" -> datasetSize.toString
+    commonResults += s"loadNumber" -> datasetSize.toString
     commonResults += s"dataSet" -> s"berlinsparql $datasetSize"
 
     val queriesObject = Class.forName(queriesObjectName).newInstance.asInstanceOf[BerlinSparqlQueries]
@@ -74,74 +79,47 @@ class BerlinSparqlEvaluation extends SlurmDeployableAlgorithm {
     println(s"Queries Object: $queriesObjectName")
     println(s"Starting warm-up... total $warmupRuns")
 
-    /*for (i <- 1 to (warmupRuns / 20)) {
-      println(s"Running warmup $i/$warmupRuns")
-      for ((queryId, listOfSubQueryIds) <- queriesObject.warmupQueries) {
-        val listOfQueries = queries(queryId)
-        for (subQueryId <- listOfSubQueryIds) {
-          val query = listOfQueries(subQueryId)
-          println(s"Running warmup for query $queryId-$subQueryId. Awaiting idle")
-          val result = executeEvaluationRun(query, 0, s"${queryId.toString}-${subQueryId.toString}", tr, commonResults)
-        }
-      }
-    }*/
-
-    var subWarmUpRun = 1
-    //var totalWarmups = 1
-    for (i <- 1 to (warmupRuns / 20)) {
-      for ((queryId, listOfSubQueryIds) <- queriesObject.warmupQueries) {
-        val listOfQueries = queries(queryId)
-        for (subQueryId <- listOfSubQueryIds) {
-          //if (totalWarmups <= warmupRuns) {
-          val query = listOfQueries(subQueryId)
-          println(s"Running warmup $subWarmUpRun for query $queryId-$subQueryId.")
-          val result = executeEvaluationRun(query, 0, s"${queryId.toString}-${subQueryId.toString}", tr, commonResults)
-          //totalWarmups += 1
-          //}
-          subWarmUpRun += 1
+    def warmup {
+      var subWarmUpRun = 1
+      if (warmupRuns != 0) {
+        for (i <- 1 to (warmupRuns / 20)) {
+          for ((queryId, listOfSubQueryIds) <- queriesObject.warmupQueries) {
+            val listOfQueries = queries(queryId)
+            for (subQueryId <- listOfSubQueryIds) {
+              val query = listOfQueries(subQueryId)
+              println(s"Running warmup $subWarmUpRun for query $queryId-$subQueryId.")
+              val result = executeEvaluationRun(query, 0, s"${queryId.toString}-${subQueryId.toString}", tr, commonResults)
+              subWarmUpRun += 1
+            }
+          }
         }
       }
     }
 
+    val warmupTime = measureTime(warmup)
+    commonResults += s"warmupTime" -> warmupTime.toString
+    
     println(s"Finished warm-up.")
     JvmWarmup.sleepUntilGcInactiveForXSeconds(60, 180)
     val resultReporter = new GoogleDocsResultHandler(spreadsheetUsername, spreadsheetPassword, spreadsheetName, worksheetName)
 
     for ((queryId, listOfSubQueryIds) <- queriesObject.queriesWithResults) {
       val listOfQueries = queries(queryId)
-      var queryRuns = 1
+      var queryRun = 1
       for (subQueryId <- listOfSubQueryIds) {
-        if (queryRuns <= 11) {
+        if (queryRun <= 11) {
           val query = listOfQueries(subQueryId)
           println(s"Running evaluation for query $queryId-$subQueryId.")
-          val result = executeEvaluationRun(query, queryRuns, s"${queryId.toString} ${queryRuns.toString} ${subQueryId.toString}", tr, commonResults)
+          val result = executeEvaluationRun(query, queryRun, s"${queryId.toString}", tr, commonResults)
           resultReporter(result)
           println(s"Done running evaluation for query $queryId-$subQueryId. Awaiting idle")
           JvmWarmup.sleepUntilGcInactiveForXSeconds(10, 30)
           println("Idle")
-          queryRuns += 1
+          queryRun += 1
         }
       }
     }
-
-    /*
-    val resultReporter = new GoogleDocsResultHandler(spreadsheetUsername, spreadsheetPassword, spreadsheetName, worksheetName)
-
-    for ((queryId, listOfQueries) <- queries) {
-      //var subQueryId = 1
-      for (subQueryId <- 1 to 25) {
-        //for (q <- queries) {
-        val q = listOfQueries(subQueryId)
-        println(s"Running evaluation for query $queryId-$subQueryId.")
-        val result = executeEvaluationRun(q, 1, s"${queryId.toString} ${subQueryId}", tr, commonResults)
-        resultReporter(result)
-        println(s"Done running evaluation for query $queryId-$subQueryId. Awaiting idle")
-        JvmWarmup.sleepUntilGcInactiveForXSeconds(10, 30)
-        println("Idle")
-        //subQueryId += 1
-      }
-    }*/
-
+    
     tr.shutdown
   }
 
@@ -162,21 +140,27 @@ class BerlinSparqlEvaluation extends SlurmDeployableAlgorithm {
     runResult += ((s"totalMemoryBefore", bytesToGigabytes(Runtime.getRuntime.totalMemory).toString))
     runResult += ((s"freeMemoryBefore", bytesToGigabytes(Runtime.getRuntime.freeMemory).toString))
     runResult += ((s"usedMemoryBefore", bytesToGigabytes(Runtime.getRuntime.totalMemory - Runtime.getRuntime.freeMemory).toString))
+    val isColdRun = (queryRun == 1) 
+    runResult += (("isColdRun", isColdRun.toString))
+    
+    val dict = tr.dictionary
     val startTime = System.nanoTime
     val queryOption = Sparql(queryString)(tr)
     val query = queryOption.get
     val resultIterator = query.encodedResults
-    //val numberOfResults = resultIterator.toList.size
-    
-    var numberOfResults = 0
 
+    var numberOfResults = 0
+    var stringLength = 0
     while (resultIterator.hasNext) {
-      numberOfResults += 1
-      val next = resultIterator.next
-      for (binding <- next) {
-        tr.dictionary.decode(binding)
+      val res = resultIterator.next
+      var arrayIndex = 0
+      while (arrayIndex < res.length) {
+        stringLength += dict.unsafeDecode(res(arrayIndex)).length()
+        arrayIndex += 1
       }
+      numberOfResults += 1
     }
+
     //val (numberOfResults, topKEntities) = transformResults(tr, query, resultIterator)
     val finishTime = System.nanoTime
     //println("Number of results: " + numberOfResults)
@@ -191,6 +175,8 @@ class BerlinSparqlEvaluation extends SlurmDeployableAlgorithm {
     runResult += ((s"queryId", queryDescription))
     runResult += ((s"queryRunId", queryRun.toString))
     runResult += ((s"results", numberOfResults.toString))
+    runResult += ((s"resultLength", stringLength.toString))
+    runResult += ((s"query", queryString))
     runResult += ((s"executionTime", executionTime.toString))
     runResult += ((s"totalMemory", bytesToGigabytes(Runtime.getRuntime.totalMemory).toString))
     runResult += ((s"freeMemory", bytesToGigabytes(Runtime.getRuntime.freeMemory).toString))
