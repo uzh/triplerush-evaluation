fafe5fa832584549acde089ad04c5ce799e6468f
Bibek <paudel.bibek@gmail.com>
2014-05-12 09:35:31 +0200

    We used this for parallel evaluation of LUBM on TR with naive optimizer.

---
+++ src/main/scala/com/signalcollect/triplerush/evaluation/LUBMEvaluationParallelNaive.scala
@@ -0,0 +1,169 @@
+package com.signalcollect.triplerush.evaluation
+import com.signalcollect.triplerush.optimizers.CleverOptimizerCreator
+
+import java.io.File
+import java.lang.management.GarbageCollectorMXBean
+import java.lang.management.ManagementFactory
+import java.util.Date
+import scala.Option.option2Iterable
+import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.JavaConversions.collectionAsScalaIterable
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+import scala.io.Source
+import scala.reflect.runtime.universe
+import com.signalcollect.GraphBuilder
+import com.signalcollect.deployment.TorqueDeployableAlgorithm
+import com.signalcollect.triplerush.TripleRush
+import com.signalcollect.triplerush.optimizers.Optimizer
+import akka.actor.ActorRef
+import com.signalcollect.triplerush.TriplePattern
+import com.signalcollect.triplerush.evaluation.lubm.FileOperations._
+import com.signalcollect.triplerush.sparql.Sparql
+import com.signalcollect.triplerush.Dictionary
+
+class LUBMEvaluationParallelNaive extends TorqueDeployableAlgorithm {
+  import SlurmEvalHelpers._
+
+  def execute(parameters: Map[String, String], nodeActors: Array[ActorRef]) {
+
+    println("TRIPLERUSH EVAL")
+    println(s"Received parameters $parameters")
+    val evaluationDescription = parameters("evaluationDescription")
+    val spreadsheetUsername = parameters("spreadsheetUsername")
+    val spreadsheetPassword = parameters("spreadsheetPassword")
+    val spreadsheetName = parameters("spreadsheetName")
+    val worksheetName = parameters("worksheetName")
+    val datasetSize = parameters("universities")
+    val warmupRuns = parameters("jitRepetitions").toInt
+
+    val graphBuilder = GraphBuilder.withPreallocatedNodes(nodeActors)
+    val tr = new TripleRush(graphBuilder, optimizerCreator = CleverOptimizerCreator)
+    println("TripleRush has been started.")
+
+    var commonResults = parameters
+    commonResults += "numberOfNodes" -> tr.graph.numberOfNodes.toString
+    commonResults += "jitRepetitions" -> warmupRuns.toString
+    commonResults += "numberOfWorkers" -> tr.graph.numberOfWorkers.toString
+    commonResults += "java.runtime.version" -> System.getProperty("java.runtime.version")
+
+    val ntriplesFileLocation = s"lubm$datasetSize-nt"
+
+    val loadingTime = measureTime {
+      loadLubmFromNTriples(datasetSize.toInt, tr)
+      tr.awaitIdle
+    }
+
+    println(s"Finished loading")
+
+    val optimizerInitialisationTime = measureTime {
+      tr.prepareExecution
+    }
+
+    println(s"Finished optimizer initialization")
+
+    JvmWarmup.sleepUntilGcInactiveForXSeconds(60, 180)
+
+    commonResults += ((s"optimizerInitialisationTime", optimizerInitialisationTime.toString))
+    commonResults += ((s"optimizerName", "CleverOptimizer"))
+    commonResults += (("loadingTime", loadingTime.toString))
+    commonResults += s"loadNumber" -> datasetSize.toString
+    commonResults += s"dataSet" -> s"LUBM $datasetSize"
+
+    val queries = LubmQueries.SparqlQueries
+
+    println(s"Starting warm-up... total $warmupRuns")
+
+    def warmup {
+      if (warmupRuns != 0) {
+        for (i <- (1 to warmupRuns / 7)) {
+          println(s"Running warmup $i/$warmupRuns")
+          for (query <- queries) {
+            executeEvaluationRun(query, 0, "warmup", tr, commonResults)
+            tr.awaitIdle
+          }
+        }
+        println(s"Finished warm-up.")
+        JvmWarmup.sleepUntilGcInactiveForXSeconds(60)
+      }
+    }
+
+    val warmupTime = measureTime(warmup)
+    commonResults += s"warmupTime" -> warmupTime.toString
+
+    println(s"Finished warm-up.")
+    JvmWarmup.sleepUntilGcInactiveForXSeconds(60, 180)
+    val resultReporter = new GoogleDocsResultHandler(spreadsheetUsername, spreadsheetPassword, spreadsheetName, worksheetName)
+
+    for (queryId <- queries.size to 1 by -1) {
+      val query = queries(queryId - 1)
+      for (queryRun <- 1 to 10) {
+        println(s"Running evaluation for query $queryId.")
+        val result = executeEvaluationRun(query, queryRun, queryId.toString, tr, commonResults)
+        resultReporter(result)
+        println(s"Done running evaluation for query $queryId. Awaiting idle")
+        tr.awaitIdle
+        println("Idle")
+
+        JvmWarmup.sleepUntilGcInactiveForXSeconds(10, 30)
+      }
+    }
+    tr.shutdown
+  }
+
+  def executeEvaluationRun(queryString: String, queryRun: Int, queryDescription: String, tr: TripleRush, commonResults: Map[String, String]): Map[String, String] = {
+    val gcs = ManagementFactory.getGarbageCollectorMXBeans.toList
+    val compilations = ManagementFactory.getCompilationMXBean
+    val javaVersion = ManagementFactory.getRuntimeMXBean.getVmVersion
+    val jvmLibraryPath = ManagementFactory.getRuntimeMXBean.getLibraryPath
+    val jvmArguments = ManagementFactory.getRuntimeMXBean.getInputArguments
+    var runResult = commonResults
+    runResult += (("javaVmVersion", javaVersion))
+    runResult += (("jvmLibraryPath", jvmLibraryPath))
+    runResult += (("jvmArguments", jvmArguments.mkString(" ")))
+    val date: Date = new Date
+    val gcTimeBefore = getGcCollectionTime(gcs)
+    val gcCountBefore = getGcCollectionCount(gcs)
+    val compileTimeBefore = compilations.getTotalCompilationTime
+    runResult += ((s"totalMemoryBefore", bytesToGigabytes(Runtime.getRuntime.totalMemory).toString))
+    runResult += ((s"freeMemoryBefore", bytesToGigabytes(Runtime.getRuntime.freeMemory).toString))
+    runResult += ((s"usedMemoryBefore", bytesToGigabytes(Runtime.getRuntime.totalMemory - Runtime.getRuntime.freeMemory).toString))
+
+    var numberOfResults = 0
+    val startTime = System.nanoTime
+    val queryOption = Sparql(queryString)(tr)
+    val query = queryOption.get
+    val resultIterator = query.resultIterator
+
+    while (resultIterator.hasNext) {
+      val res = resultIterator.next
+      numberOfResults += 1
+    }
+
+    val finishTime = System.nanoTime
+    val executionTime = roundToMillisecondFraction(finishTime - startTime)
+    val gcTimeAfter = getGcCollectionTime(gcs)
+    val gcCountAfter = getGcCollectionCount(gcs)
+    val gcTimeDuringQuery = gcTimeAfter - gcTimeBefore
+    val gcCountDuringQuery = gcCountAfter - gcCountBefore
+    val compileTimeAfter = compilations.getTotalCompilationTime
+    val compileTimeDuringQuery = compileTimeAfter - compileTimeBefore
+    runResult += ((s"queryId", queryDescription))
+    runResult += ((s"queryRunId", queryRun.toString))
+    runResult += ((s"results", numberOfResults.toString))
+    runResult += ((s"query", queryString))
+    runResult += ((s"executionTime", executionTime.toString))
+    runResult += ((s"totalMemory", bytesToGigabytes(Runtime.getRuntime.totalMemory).toString))
+    runResult += ((s"freeMemory", bytesToGigabytes(Runtime.getRuntime.freeMemory).toString))
+    runResult += ((s"usedMemory", bytesToGigabytes(Runtime.getRuntime.totalMemory - Runtime.getRuntime.freeMemory).toString))
+    runResult += ((s"executionHostname", java.net.InetAddress.getLocalHost.getHostName))
+    runResult += (("gcTimeAfter", gcTimeAfter.toString))
+    runResult += (("gcCountAfter", gcCountAfter.toString))
+    runResult += (("gcTimeDuringQuery", gcTimeDuringQuery.toString))
+    runResult += (("gcCountDuringQuery", gcCountDuringQuery.toString))
+    runResult += (("compileTimeAfter", compileTimeAfter.toString))
+    runResult += (("compileTimeDuringQuery", compileTimeDuringQuery.toString))
+    runResult += s"date" -> date.toString
+    runResult
+  }
+}
