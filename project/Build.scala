721986901c4e77cf3f718a3d4ca6161a680c1693
Bibek <paudel.bibek@gmail.com>
2014-07-07 13:47:46 +0200

    Added dependency on sc-torque and sc-slurm.

--- project/Build.scala
+++ project/Build.scala
@@ -3,6 +3,8 @@ import Keys._
 
 object GraphsBuild extends Build {
   lazy val triplerush = ProjectRef(file("../triplerush"), id = "triplerush")
+  lazy val scCore = ProjectRef(file("../signal-collect-torque"), id = "signal-collect-torque")
+  lazy val scSlurm = ProjectRef(file("../signal-collect-slurm"), id = "signal-collect-slurm")
   val scTriplerush = Project(id = "triplerush-evaluation",
-    base = file(".")) dependsOn (triplerush)
+    base = file(".")) dependsOn (triplerush, scCore, scSlurm)
 }
