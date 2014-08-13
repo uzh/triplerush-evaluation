99f893a7d430ba8031233d755f6b30fd05598389
Bibek <paudel.bibek@gmail.com>
2014-08-13 08:52:38 +0200

    For profiling Heuristics Optimizer.

--- src/test/scala/com/signalcollect/triplerush/HeuristicsOptimizerBenchmark.scala
+++ src/test/scala/com/signalcollect/triplerush/HeuristicsOptimizerBenchmark.scala
@@ -14,6 +14,7 @@ import com.signalcollect.triplerush.evaluation.BerlinQueriesParameterized7
 import com.signalcollect.triplerush.sparql.Sparql
 
 object HeuristicsOptimizerBenchmark extends App {
+
   def roundToMillisecondFraction(nanoseconds: Long): Double = {
     ((nanoseconds / 100000.0).round) / 10.0
   }
@@ -36,28 +37,36 @@ object HeuristicsOptimizerBenchmark extends App {
 
     val stats = new PredicateSelectivity(tr)
 
-    val clever = CleverCardinalityOptimizer
-    val withHeuristic = new ExplorationOptimizer(stats, useHeuristic = true)
-    val withoutHeuristic = new ExplorationOptimizer(stats, useHeuristic = false)
+    println("press any key")
+    readLine()
+
+    //val clever = CleverCardinalityOptimizer
+    //val withHeuristic = new ExplorationOptimizer(stats, useHeuristic = true)
+    //val withoutHeuristic = new ExplorationOptimizer(stats, useHeuristic = false)
 
     val heuristicOptimizer = new ExplorationHeuristicsOptimizer(stats)
 
-    for (i <- 1 to 100000) {
-    //for (i <- 1 to 10) {
-      println("Clever Optimizer")
+    val startTime = System.nanoTime()
+    val optimizationRuns = 10000
+    for (i <- 1 to optimizationRuns) {
+      //for (i <- 1 to 10) {
+      /*println("Clever Optimizer")
       executionTimeCleverOptimizer = math.min(executionTimeCleverOptimizer, executeQuery(tr, clever))
       println("Without Heuristics")
       executionTimeWithoutHeuristic = math.min(executionTimeWithoutHeuristic, executeQuery(tr, withoutHeuristic))
       println("With Heuristics")
       executionTimeWithHeuristic = math.min(executionTimeWithHeuristic, executeQuery(tr, withHeuristic))
-      println("With new Heuristics Optimizer")
+      println("With new Heuristics Optimizer")*/
       executionTimeWithNewHeuristic = math.min(executionTimeWithNewHeuristic, executeQuery(tr, heuristicOptimizer))
     }
+    val totalDuration = System.nanoTime() - startTime
+    println(s"execution time: ${roundToMillisecondFraction(totalDuration)} ms for $optimizationRuns runs.")
+    
   } finally {
     tr.shutdown
   }
 
-  println(s"Execution time: 0) with new heuristic: $executionTimeWithNewHeuristic, i) with Heuristic: $executionTimeWithHeuristic, ii) without Heuristic: $executionTimeWithoutHeuristic, iii) Clever: $executionTimeCleverOptimizer,  iv) difference heuristic: ${executionTimeWithHeuristic - executionTimeWithoutHeuristic}, v) difference clever: ${executionTimeWithoutHeuristic - executionTimeCleverOptimizer}")
+  //println(s"Execution time: 0) with new heuristic: $executionTimeWithNewHeuristic, i) with Heuristic: $executionTimeWithHeuristic, ii) without Heuristic: $executionTimeWithoutHeuristic, iii) Clever: $executionTimeCleverOptimizer,  iv) difference heuristic: ${executionTimeWithHeuristic - executionTimeWithoutHeuristic}, v) difference clever: ${executionTimeWithoutHeuristic - executionTimeCleverOptimizer}")
 
   def executeQuery(tr: TripleRush, optimizer: Optimizer): Double = {
     val queryString = BerlinQueriesParameterized7.queries(2)(1)
